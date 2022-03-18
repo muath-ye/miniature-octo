af32bad214d59b06b380fb28c43f10e83abdff92
muathye <muath.ye@gmail.com>
2022-03-19 00:36:01 +0300

    Update pages controller

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Core\App;
+
 class PagesController
 {
     /**
@@ -18,5 +20,17 @@ class PagesController
     public function sql()
     {
         return view('sql');
+    }
+    
+    /**
+     * Show the categories page.
+     */
+    public function categories()
+    {
+        // $data = App::get('database')->all('tbl_category');
+        // $data = App::get('database')->where('tbl_category', 'WHERE parent_category_id = 0');
+
+        // return view('categories', compact('data'));
+        return view('categories');
     }
 }
