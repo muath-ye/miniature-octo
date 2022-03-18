c7f4dd96e223527a765401e9c935b71863dd28fe
muathye <muath.ye@gmail.com>
2022-03-18 20:24:54 +0300

    Update config

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
     'database' => [
-        'name' => 'mytodo',
+        'name' => 'miniature-octo',
         'username' => 'root',
         'password' => '',
         'connection' => 'mysql:host=127.0.0.1',
