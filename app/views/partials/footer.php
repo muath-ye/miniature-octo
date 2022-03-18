c2d986d39deac12d5a3ab57733ee1266951e31a7
muathye <muath.ye@gmail.com>
2022-03-18 22:19:07 +0300

    Add copy to clipboard

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -5,13 +5,34 @@
      */
 
     /* get current url path */
-    var page_url = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
+    var page_url = window.location.href.substr(window.location.href.lastIndexOf("/") + 1);
 
     /* select element with same url path */
-    var active_page = document.querySelectorAll("a[href='/"+page_url+"']");
-    
+    var active_page = document.querySelectorAll("a[href='/" + page_url + "']");
+
     /* add active class to selected element */
     active_page[0].className += " active";
+</script>
+<script>
+    function copyToClipboard(elementId) {
+        /* Get the text field */
+        // var copyText = document.getElementById("myInput");
+        var copyText = document.getElementById(elementId);
+
+        /* Select the text field */
+        // copyText.select();
+        // copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+        /* Copy the text inside the text field */
+        // navigator.clipboard.writeText(copyText.value);
+
+        /* Copy text to clipboard */
+        navigator.clipboard.writeText(copyText.textContent);
+
+        /* Alert the copied text */
+        // alert("Copied the text: " + copyText.textContent);
+        copyText.className += " active";
+    }
 </script>
 </body>
 
