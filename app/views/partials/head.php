7f86a3d8ad9e8398ee4179e8198b3b9fd12132c3
muathye <muath.ye@gmail.com>
2022-03-18 21:10:31 +0300

    Update title

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Miniature Octo</title>
     <link href="<?= asset('css/bootstrap.rtl.min.css') ?>" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="<?= asset('css/style.css'); ?>">
 </head>
