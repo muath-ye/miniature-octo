b3c76ab2dc1d06b1b51dd9e05a224262379c3ac4
muathye <muath.ye@gmail.com>
2022-03-18 21:45:15 +0300

    Add auto active script

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -1,4 +1,18 @@
-
 <script src="<?= asset('js/bootstrap.bundle.min.js') ?>"></script>
+<script>
+    /**
+     * Add active class to the current button (highlight it).
+     */
+
+    /* get current url path */
+    var page_url = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
+
+    /* select element with same url path */
+    var active_page = document.querySelectorAll("a[href='/"+page_url+"']");
+    
+    /* add active class to selected element */
+    active_page[0].className += " active";
+</script>
 </body>
+
 </html>
