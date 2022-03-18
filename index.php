5bff7b5b986d6d6796e11f704ff4f01aba382ecd
muathye <muath.ye@gmail.com>
2022-03-18 20:25:10 +0300

    Add whoops to index.php

--- index.php
+++ index.php
@@ -5,5 +5,9 @@ require 'core/bootstrap.php';
 
 use App\Core\{Router, Request};
 
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
 Router::load('app/routes.php')
     ->direct(Request::uri(), Request::method());
